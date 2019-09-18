2d183dfd106d679ff22511cf3f115dc45f6376cc
tracey brimage <onelovenilynn@gmail.com>
2019-09-18 04:35:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,6 @@ if speak_to_grandma=="'Hi Nana, how are you?'"
   puts "HUH?! SPEAK UP,SONNY!"
 elsif "HI!"
   puts "HUH?! SPEAK UP,SONNY!"
-else "NO, NOT SINCE 1938!"
+else "WHAT DID YOU EAT TODAY?"
+  puts
 end
