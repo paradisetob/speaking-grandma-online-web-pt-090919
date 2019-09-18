8a99c7e3cd1f936011816bf914d480ad3f0cfc64
tracey brimage <onelovenilynn@gmail.com>
2019-09-18 04:33:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 speak_to_grandma="I LOVE YOU GRANDMA"
 if speak_to_grandma=="'Hi Nana, how are you?'"
   puts "HUH?! SPEAK UP,SONNY!"
-elsif ""
+elsif "HI!"
   puts "HUH?! SPEAK UP,SONNY!"
 else "NO, NOT SINCE 1938!"
 end
